053525c984506680de9b5763354ff7b0f3388534
e.kingaguirre <e.kingaguirre@gmail.com>
2024-11-20 18:13:48 +0800

    f

--- src/DataTable/utils/index.tsx
+++ src/DataTable/utils/index.tsx
@@ -1498,6 +1498,24 @@ const updateData = (data, updates) => {
   return updatedData;
 };
 
+const updateDataSource = (dataSource, deletedRows, rowKey, isPermanentDelete) => {
+  // Create a Set of rowKey values from deletedRows for efficient look-up
+  const deletedKeys = new Set(deletedRows.map(row => row[rowKey]));
+
+  // Create a new array by mapping or filtering the dataSource
+  if (isPermanentDelete) {
+    // Filter out rows where the rowKey is in deletedKeys
+    return dataSource.filter(row => !deletedKeys.has(row[rowKey]));
+  } else {
+    // Map the dataSource, updating the intentAction to "D" if the rowKey is in deletedKeys
+    return dataSource.map(row => 
+      deletedKeys.has(row[rowKey]) 
+        ? { ...row, intentAction: "D" } 
+        : { ...row }
+    );
+  }
+};
+
 export * from "./useDragDropManager";
 export * from "./useResizeManager";
 export * from "./useCheckOverflow";
